be81877056d0e9698cb486cd95dd5b59fee1afbb
Ann <anka73der@gmail.com>
2024-10-28 16:52:23 +0300

    Initial Commit

---
+++ ItogProject/ItogProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  ItogProjectApp.swift
+//  ItogProject
+//
+//  Created by Анна on 28.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct ItogProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
